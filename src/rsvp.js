e5811b5ecc43408af3425b32f9bd220c08cdb199
Miguel Angel Mansilla Revoux <punklodawn@hotmail.com>
2025-03-13 03:13:53 -0300

    prueba11

--- src/rsvp.js
+++ src/rsvp.js
@@ -21,8 +21,8 @@ app.use(cors({
 }));
 
 const supabase = createClient(
-  process.env.SUPABASE_URL,
-  process.env.SUPABASE_KEY
+"https://llilswxrkaroyatuhokc.supabase.co",
+"eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImxsaWxzd3hya2Fyb3lhdHVob2tjIiwicm9sZSI6ImFub24iLCJpYXQiOjE3Mzg5NzcwNDQsImV4cCI6MjA1NDU1MzA0NH0.1yaJySmAljR7TGzghHbCyPUDtkyoQEeH54SZVJYmFL8"
 );
 
 
